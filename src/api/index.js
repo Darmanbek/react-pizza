f1f54275f838f689851761aac63094e76dbd7ddd
Darmanbek <96102184+Darmanbek@users.noreply.github.com>
2023-11-01 15:10:27 +0500

    Update file

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://62aa0f55371180affbce7771.mockapi.io/reactdodopizza/"
+    baseURL: "https://..."
 });
 
 export default api;
